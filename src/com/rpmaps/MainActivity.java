34a2b8ca4ecae24c385fc5edcf15de46e1fcca58
suryaramadhon <suryaramadhon1995@gmail.com>
2016-04-01 17:10:11 +0700

    update mainactivity google map

--- src/com/rpmaps/MainActivity.java
+++ src/com/rpmaps/MainActivity.java
@@ -1,11 +1,14 @@
 package com.rpmaps;
 
+import com.google.android.gms.maps.GoogleMap;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+	GoogleMap map_;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
